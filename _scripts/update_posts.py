85082f5f9fdfb07f4f83ea191b305b9dc0cb1caf
Heike Leitte <leitte@rptu.de>
2025-02-24 08:56:02 +0100

    posts are fully recreated from metadata only (no manual editing in posts)

--- _scripts/update_posts.py
+++ _scripts/update_posts.py
@@ -10,15 +10,16 @@ posts_dir = pathlib.Path('_posts')
 metadata_dir.mkdir(exist_ok=True)
 posts_dir.mkdir(exist_ok=True)
 
-#existing_metadata_files = {f.name for f in metadata_dir.glob("*.yml")}
+# delete all existing posts
+for item in posts_dir.iterdir():
+    if item.is_file() or item.is_symlink():
+        item.unlink()
 
-
+# create posts anew
 for metadata_file in metadata_dir.glob("*.yml"):
     with open(metadata_file) as file:
         metadata = yaml.safe_load(file)
 
-    header_keys = ['title', 'venue', 'citationCount']
-    body_keys = ['abstract']
     header = {'layout': 'post',
               'excerpt_image': 'NO_EXCERPT_IMAGE',
               'author': format_authors(metadata.get('authors', [])),
@@ -27,16 +28,12 @@ for metadata_file in metadata_dir.glob("*.yml"):
               'venue': metadata.get('venue'),
               'citationCount': metadata.get('citationCount'),
               'doi': metadata['externalIds']['DOI'],
-              'categories': None,
-              'tags': None
+              'categories': metadata.get('categories', None),
+              'tags': metadata.get('tags', None)
               }
-    # print('---')
-    # print(yaml.safe_dump(header))
-    # print('---')
-    # print(metadata['abstract'])
-    # print(metadata_file.with_suffix('.md').name)
 
-    # jekyll only allow '-' as special characters in filenames
+    # format the post title to match metadata and comply with jekyll
+    # jekyll only allows '-' as special characters in filenames
     post_filename = metadata_file.with_suffix('.md').name
     name, ext = post_filename.rsplit('.', 1)
     filename_jekyllFormat = re.sub(r'[^a-zA-Z0-9]', '-', name)
